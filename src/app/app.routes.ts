fe6a9879b42c70c6874657fb5613db4806579e79
Kurakina Anastasia <anastasiya.kurakina@zazmic.com>
2025-01-28 21:57:21 +0200

    Initial setup for the Application
    Add GitHub page deployment
    Add Angular Material
    Add new Forecast page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'weather',
+    loadChildren:
+      () => import('./pages/weather-forecast-page/weather-routing')
+        .then(m => m.FORECAST_ROUTING)
+  },
+  {
+    path: '**',
+    redirectTo: 'weather',
+    pathMatch: 'full',
+  }
+];
